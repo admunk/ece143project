02015850af57098291f0f46d5a9bbde8293a2dcb
Allen Munk <allenmunk@gmail.com>
2020-12-04 15:28:33 -0800

    Added visualization code

--- link_information_parser.py
+++ link_information_parser.py
@@ -1,4 +1,8 @@
+import matplotlib.pyplot as plt
+import numpy as np
 import pafy
+import pandas as pd
+import seaborn as sns
 
 youtube_links = {}
 def get_link_ratio(x):
@@ -8,7 +12,7 @@ def get_link_ratio(x):
     '''
     input_file = open(x, 'r')
     input_file.readline()
-    output_file = open('output.txt','w')
+    #output_file = open('output.txt','w')
     output_dict = {}
     comment_dict = {}
     link_dict = {}
@@ -19,7 +23,6 @@ def get_link_ratio(x):
         line = input_file.readline()
         if not line:
             break
-
         personality_type, sep, data = line.partition(',')
         data = data[1:len(data) - 1]
         comments = data.split('|||')
@@ -30,15 +33,15 @@ def get_link_ratio(x):
                     start_index = comment.find(youtube_link_starter)
                     youtube_link = comment[start_index:start_index + len(youtube_link_starter) + youtube_link_code_length]
                     youtube_links.setdefault(personality_type, []).append(youtube_link)
-                    output_file.write('{0}\n'.format(youtube_link))
+                    #output_file.write('{0}\n'.format(youtube_link))
                     comment = comment[:start_index] + comment[start_index+len(youtube_link_starter) + youtube_link_code_length:]
                 link_count += 1
         comment_dict[personality_type] = comment_dict.get(personality_type,0) + len(comments)
         link_dict[personality_type] = link_dict.get(personality_type,0) + link_count
     for key in comment_dict.keys():
-        output_dict[key] = link_dict[key]/comment_dict[key]
+        output_dict[key] = 100 * link_dict[key]/comment_dict[key]
     input_file.close()
-    output_file.close
+    #output_file.close
     return output_dict 
 
 def make_video_data_files(x):
@@ -51,10 +54,19 @@ def make_video_data_files(x):
         for link in v:
             try: 
                 video = pafy.new(link)
-                output_file.write("{0},{1}\n".format(video.title.replace(",",""), video.viewcount))
+                output_file.write("{0},{1},{2}\n".format(video.title.replace(",",""), video.viewcount,video.category))
             except:
                 continue
         output_file.close()
+
+def make_one_file(x, name):
+    output_file = open("link_data/{0}.csv".format(name), "w")
+    for v in x.get(name,[]):
+        try: 
+            video = pafy.new(v)
+            output_file.write("{0},{1},{2}\n".format(video.title.replace(",",""), video.viewcount,video.category))
+        except:
+            continue
 
 def get_avg_views(x):
     '''
@@ -71,7 +83,7 @@ def get_avg_views(x):
             if not line:
                 break
             comma_count = line.count(',')
-            count += int(line.split(',')[comma_count])
+            count += int(line.split(',')[comma_count - 1])
             index += 1
         if(index < 30):
             count = 0
@@ -97,7 +109,9 @@ def get_link_categories(x):
 
 #Get link reation, print sorted map
 x = get_link_ratio('mbti_1.csv')
-print({k: v for k, v in sorted(x.items(), key=lambda item: item[1])})
+x = {k: v for k, v in sorted(x.items(), key=lambda item: item[1])}
+# print(x)
+# print({k: v for k, v in sorted(x.items(), key=lambda item: item[1])})
 '''
 {'ESFJ': 0.013875123885034688, 'ESTJ': 0.016137428422696512, 'ENTP': 0.026983797873285743, 
 'ENTJ': 0.02847511753747893, 'ESFP': 0.028893905191873587, 'ESTP': 0.03135808162324187, 
@@ -106,17 +120,32 @@ print({k: v for k, v in sorted(x.items(), key=lambda item: item[1])})
 'ISTP': 0.03921687477269972, 'INTP': 0.03974178885399075, 'INFP': 0.042340416054167225, 
 'ISFP': 0.05169230769230769}
 '''
-# #create data files WARNING function makes ~11000 http requests, runtime ~ 2 hours
+
+# plt.figure(figsize=(20,10))
+# sns.barplot(x=np.array(list(x.values())), y=np.array(list(x.keys())))
+# plt.ylabel('Type', fontsize=16)
+# plt.xlabel('Number of Links (Per 100 Posts)', fontsize=16)
+# plt.xticks(fontsize=16, rotation=0)
+# plt.yticks(fontsize=16, rotation=0)
+# plt.title('Average Links Per 100 Posts By Personality')
+# plt.show()
+#create data files WARNING function makes ~11000 http requests, runtime ~ 2 hours
 # make_video_data_files(youtube_links)
+# make_one_file(youtube_links, "ISTP")
 
 #Find average views per video, sort results
 y = (get_avg_views(youtube_links))
-print({k: v for k, v in sorted(y.items(), key=lambda item: item[1])})
+y = {k: v for k, v in sorted(y.items(), key=lambda item: item[1])}
 '''
 {'ESFJ': 0, 'ESFP': 0, 'ESTJ': 0, 'INTJ': 17136182, 'INFP': 18424824, 'INTP': 21762186, 
 'ISTJ': 22282518, 'ISTP': 25928740, 'INFJ': 29320080, 'ENFP': 30275269, 'ENFJ': 47571205, 
 'ISFJ': 48570312, 'ISFP': 48798584, 'ENTP': 50213236, 'ENTJ': 52580978, 'ESTP': 54378005}
 '''
-# #Test ESTP's categories distribution
-# y = get_link_categories(youtube_links)
-# print({k: v for k, v in sorted(y.items(), key=lambda item: item[1])})
+# plt.figure(figsize=(20,10))
+# sns.barplot(x=np.array(list(y.values())), y=np.array(list(y.keys())))
+# plt.ylabel('Type', fontsize=16)
+# plt.xlabel('Average Views', fontsize=16)
+# plt.xticks(fontsize=16, rotation=0)
+# plt.yticks(fontsize=16, rotation=0)
+# plt.title('Average View Per Link By Personality')
+# plt.show()
--- spotify_search.py
+++ spotify_search.py
@@ -1,7 +1,10 @@
+import matplotlib.pyplot as plt
 import spotipy
 from spotipy.oauth2 import SpotifyClientCredentials
 
-
+type_list = ["ENFJ", "ENFP", "ENTJ", "ENTP", "ESFJ", "ESFP", "ESTJ", "ESTP", "INFJ", "INFP", "INTJ", "INTP", "ISFJ", "ISFP", "ISTJ", "ISTP"]
+genre_list = ["rock","hip hop","pop","electronic","r&b","dance","folk","jazz","classic","soundtrack"]
+color_list = ["#fff100","#ff8c00","#e81123","#ec008c","#68217a","#00188f","#bad80a","#00b294","#009e49","#00bcf2","#9f5f80","#fcf8ec"]
 def find_genre(genre_list):
     if len(genre_list) == 0:
         return 'other'
@@ -27,34 +30,75 @@ def find_genre(genre_list):
         elif 'soundtrack' in genre or 'broadway' in genre:
             return 'soundtrack'
     print(genre_list)
+    if(len(generator) == 0):
+        return 'unlisted'
     return 'other'
     
 
-def get_genres(x):
+def make_genre_files():
     sp = spotipy.Spotify(auth_manager=SpotifyClientCredentials(client_id="cfdbf9aecc71457f9421a63b45d888f5",
                                                             client_secret="6e73af200e5e4f9eb67010e6df985cb3"))
-    input_file = open('link_data/INFJ.csv', 'r')
-    genre_dict = {}
-    success_count = 0
-    total_count = 0
-    while True:
-        line = input_file.readline()
-        if not line:
-            break
-        components = line.split(',')   
-        if components[2] == 'Music\n':
-            total_count += 1
-            try:
-                results = sp.search(q=components[0], limit=1, type='track')
-                artist = results['tracks']['items'][0]['album']['artists'][0]['id'] 
-                genres = sp.artist(artist)['genres']
-                genre = find_genre(genres)
-                genre_dict[genre] = genre_dict.get(genre,0) + 1
-                success_count += 1
-            except:
-                continue
-    return genre_dict
-print(get_genres('x'))    
-'''
-{'rock': 63, 'pop': 44, 'electronic': 7, 'jazz': 12, 'hip hop': 13, 'folk': 15, 'other': 33, 'classic': 7, 'soundtrack': 4, 'dance': 13, 'r&b': 3}
-'''
+    
+    for personality in type_list:    
+        input_file = open('link_data/{0}.csv'.format(personality), 'r')
+        output_file = open('link_data/spotify-{0}.csv'.format(personality), 'w')
+        genre_dict = {}
+        for genre in genre_list:
+            genre_dict[genre] = 0
+        success_count = 0
+        total_count = 0
+        while True:
+            line = input_file.readline()
+            if not line:
+                break
+            components = line.split(',')   
+            if components[2] == 'Music\n':
+                total_count += 1
+                try:
+                    results = sp.search(q=components[0], limit=1, type='track')
+                    artist = results['tracks']['items'][0]['album']['artists'][0]['id']
+                    name = results['tracks']['items'][0]['album']['artists'][0]['name']
+                    genres = sp.artist(artist)['genres']
+                    genre = find_genre(genres)
+                    output_file.write("{0},{1},{2}\n".format(components[0], name,genre))
+                    genre_dict[genre] = genre_dict.get(genre,0) + 1
+                    success_count += 1
+                except:
+                    continue
+        input_file.close()
+        output_file.write("Genres:\n")
+        for k,v in genre_dict.items():
+            output_file.write("{0},{1}\n".format(k,v))
+        output_file.close()
+
+def make_genre_charts():
+    for personality in type_list:
+        input_file = open("link_data/spotify-{0}.csv".format(personality)) 
+        labels = []
+        count = []
+        colors = []
+        genre_count_dict = {}
+        color_index = 0
+        while True:
+            line = input_file.readline()
+            if not line:
+                break
+            if line.count(',') == 1:
+                components = line.split(',')
+                genre_count_dict[components[0]] = components[1]
+                if(int(components[1]) > 0):   
+                    labels.append(components[0])
+                    count.append(components[1])
+                    colors.append(color_list[color_index])
+                color_index += 1
+        input_file.close()
+        if len(labels) > 0:
+            fig, ax = plt.subplots(figsize=(20, 10), subplot_kw=dict(aspect="equal"))
+            wedges, texts, autotexts = ax.pie(count, labels=labels,autopct='%1.1f%%',
+                                    textprops=dict(color="w"),pctdistance=0.9,startangle=90,colors = colors)
+            ax.legend(wedges, labels, title="Personality Types", bbox_to_anchor=(1, 0, 0.5, 1))
+            plt.setp(autotexts, size=8,weight="bold")
+            ax.set_title("{0}'s Genres".format(personality),size=16)
+            plt.show()
+# make_genre_files()
+make_genre_charts()
